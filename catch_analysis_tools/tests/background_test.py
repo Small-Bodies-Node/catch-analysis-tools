da77291d9d14731280d3e76324cfbe688ee42e24
bensharkey <bensharke@gmail.com>
2025-05-02 14:32:56 -0400

    responding to comments

--- catch_analysis_tools/tests/background_test.py
+++ catch_analysis_tools/tests/background_test.py
@@ -4,24 +4,22 @@ from pytest import approx
 
 from ..background import *
 
-def test_global_subtraction_data():
+def test_global_subtraction():
     data = np.ones((100,100))
     data[40:50,40:50] = 5*data[40:50,40:50]
     data_sub, bkg = global_subtraction(data)
-    assert np.mean(data_sub) == 0.04
 
-def test_global_subtraction_bkg():
-    data = np.ones((100,100))
-    data[40:50,40:50] = 5*data[40:50,40:50]
-    data_sub, bkg = global_subtraction(data)
+    errors = []
+    # test some basic values checked manually against above image values
+    assert np.mean(data_sub) == 0.04
     assert np.mean(bkg.background) == 1.0
+    
 
 def test_get_background():
     data = np.ones((100,100))
     data[40:50,40:50] = 5*data[40:50,40:50]
     bkg = get_background(data)
     assert np.mean(bkg.background) == 1.0
-
 
 def test_calc_annulus_bkg():
     import photutils.datasets
